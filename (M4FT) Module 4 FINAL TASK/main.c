ae39692c0633b88b944f82afacdb7440ae0a3748
Justin <73634104+emnace13@users.noreply.github.com>
2020-12-05 17:39:25 +0800

    Update main.c

--- (M4FT) Module 4 FINAL TASK/main.c
+++ (M4FT) Module 4 FINAL TASK/main.c
@@ -4,18 +4,18 @@
 
 int main()
 {
-   int i,j,spc,rows,k,t=1;
-   printf("Input number of rows : ");
-   scanf("%d",&rows);
-   spc=rows+4-1;
-   for(i=1;i<=rows;i++)
+   int i, j, spc, rows, k, t = 1;
+   printf("Number of rows : ");
+   scanf("%d", &rows);
+   spc = rows + 4 - 1;
+   for(i = 1; i <= rows; i++)
    {
          for(k=spc;k>=1;k--)
             {
               printf(" ");
             }
-	   for(j=1;j<=i;j++)
-	   printf("%d ",t++);
+	   for(j = 1; j <= i; j++)
+	   printf("%d ", t++);
 	printf("\n");
     spc--;
    }
